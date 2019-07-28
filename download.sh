4a8fa1c19cf4f201b563c9e6214119a58ebf36d7
spihill <spihill.in@gmail.com>
2019-07-29 02:24:22 +0900

    new ModComb and modify download.sh

--- download.sh
+++ download.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 files='files.txt'
+user='spihill'
+repo='library'
 
- 	curl -O -H "Authorization: token $MY_GITHUB_SOURCE_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/spihill/library/contents/$files
+if [ -z "$MY_GITHUB_SOURCE_TOKEN" ]; then
+	   echo "Please set MY_GITHUB_SOURCE_TOKEN"
+	   exit 1
+fi
 
-exec 3< $files
+	echo "Downloading $files"
+ 	curl -s -S -O -H "Authorization: token $MY_GITHUB_SOURCE_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/$user/$repo/contents/$files
+
+tmp=.cpplib.tmp
+tail -n +2 $files > $tmp
+
+exec 3< $tmp
 
 while read line 0<&3
 do
-	echo $line
- 	curl -O -H "Authorization: token $MY_GITHUB_SOURCE_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/spihill/library/contents/$line
-
+	echo "Downloading $line"
+ 	curl -s -S -O -H "Authorization: token $MY_GITHUB_SOURCE_TOKEN" -H 'Accept: application/vnd.github.v3.raw' -L https://api.github.com/repos/$user/$repo/contents/$line &
+	sleep 0.2
 done
 
+wait
+
+rm $tmp
