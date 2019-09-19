54feee7cd24ed351b3f977f8071f862581273b1b
spihill <spihill.in@gmail.com>
2019-09-19 16:06:54 +0900

    fixed

--- register.sh
+++ register.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ -z "$VSCODE_SNIPPETS_DIR" ]; then
-	echo "Please set VSCODE_SNIPPTES_DIR"
+if [ -z "$MY_VSCODE_SNIPPETS_DIR" ]; then
+	echo "Please set MY_VSCODE_SNIPPTES_DIR"
 	exit 1
 fi
 
-python3 cpp2json.py && cp -i cpp.json $VSCODE_SNIPPETS_DIR
+python3 cpp2json.py && cp -i cpp.json $MY_VSCODE_SNIPPETS_DIR
